d4f83e0826ecc01ecccd8a6f89345bc3107cbc65
curlyz <curly.saigonese@gmail.com>
2024-03-18 13:17:36 +0700

    deploy

---
+++ _next/static/chunks/pages/layout-a36cd85c8944e987.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[777],{10836:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout",function(){return u(12006)}])}},function(n){n.O(0,[838,396,660,817,509,578,6,774,888,179],(function(){return _=10836,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
+//# sourceMappingURL=layout-a36cd85c8944e987.js.map
