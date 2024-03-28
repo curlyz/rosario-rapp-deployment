91f637fcf6b7132ac57352b50bd017de68af020b
curlyz <curly.saigonese@gmail.com>
2024-03-29 03:06:24 +0700

    deploy

---
+++ _next/static/chunks/pages/formula-df41296134b7b9c2.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[486],{30340:function(n,i,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/formula",function(){return u(60314)}])},60314:function(n,i,u){"use strict";u.r(i);var e=u(85893);u(67294);i.default=()=>(0,e.jsx)("div",{children:(0,e.jsx)("p",{children:"This is a simple page for the formula section."})})}},function(n){n.O(0,[774,888,179],(function(){return i=30340,n(n.s=i);var i}));var i=n.O();_N_E=i}]);
+//# sourceMappingURL=formula-df41296134b7b9c2.js.map
---
+++ _next/static/chunks/pages/layout-8d2f94737bdbe235.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[777],{10836:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout",function(){return u(34546)}])}},function(n){n.O(0,[838,396,660,296,817,509,456,262,282,546,774,888,179],(function(){return _=10836,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
+//# sourceMappingURL=layout-8d2f94737bdbe235.js.map
